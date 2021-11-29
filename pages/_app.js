ef4b9b6994aea150b2eba6d2ad76d8bb0d10df5e
Karl L. Hughes <khughes.me@gmail.com>
2021-11-29 14:53:20 +0000

    Added logrocket

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
 import "purecss";
 import "../styles/globals.css";
 import Layout from "../components/layout";
+import LogRocket from 'logrocket';
+LogRocket.init('cm06dj/writer-portal');
 
 function MyApp({ Component, pageProps }) {
+  try {
+    if (typeof localStorage !== 'undefined' && localStorage && localStorage.getItem("COTTER_USER") !== null) {
+      LogRocket.identify(JSON.parse(localStorage.getItem("COTTER_USER")).identifier);
+    }
+  } catch (e) {
+    console.error(e);
+  }
   return (
     <Layout>
       <Component {...pageProps} />
