f9460a0271ac44d8f1dd9d10776bc01365e8f2dd
Aniket Bhattacharyea <aniket@abhattacharyea.dev>
2022-09-16 18:50:22 +0530

    Add "Nothing to see here" pages

--- components/assignments/assignments.js
+++ components/assignments/assignments.js
@@ -5,6 +5,7 @@ import * as localizedFormat from "dayjs/plugin/localizedFormat";
 import * as utc from "dayjs/plugin/utc";
 import { useAssignments } from "../../data/use-assignments";
 import { Error } from "../error";
+import Link from "next/link";
 dayjs.extend(localizedFormat);
 dayjs.extend(utc);
 
@@ -75,92 +76,117 @@ export default function Assignments({ type }) {
       ) : (
         ""
       )}
-      <table className="table w-full">
-        <thead>
-          <tr>
-            <th></th>
-            <th>Title</th>
-            <th>Status</th>
-            <th>Due Date</th>
-            <th>Payment</th>
-          </tr>
-        </thead>
-        <tbody>
-          {assignments.map((assignment) => (
-            <tr key={assignment.id}>
-              <th className="text-xs">{serial++}</th>
-              <td>
-                <div className="flex flex-col flex-wrap break-words">
-                  <div className="font-bold text-sm">
-                    <a href={"/assignments/" + assignment.id}>
-                      {assignment.title}
-                    </a>
-                  </div>
-                  <div className="text-xs opacity-50">
-                    For {assignment.client_name}
-                  </div>
-                </div>
-              </td>
-              <td>
-                {assignment.published_url ? (
-                  <a
-                    className="link badge badge-success badge-lg text-sm text-white"
-                    href={assignment.published_url}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    Published
-                  </a>
-                ) : (
-                  <span
-                    className={`badge ${
-                      assignment.status === assignmentStatuses.assigning
-                        ? "badge-info"
-                        : "badge-primary"
-                    } badge-lg text-sm`}
-                  >
-                    {assignment.status}
-                  </span>
-                )}
-              </td>
-              <td>
-                <span className="text-sm">
-                  {dayjs(assignment.writer_due_date).format("LL")}
-                </span>
-              </td>
-              <td>
-                <div className="text-sm">
-                  ${assignment.writer_payout}{" "}
-                  {assignment.writer_paid_date ? (
-                    <div
-                      className="tooltip tooltip-left"
-                      data-tip={
-                        "Payment initiated on " +
-                        dayjs(assignment.writer_paid_date).format("LL")
-                      }
-                    >
-                      <span className="badge badge-ghost badge-lg text-sm ml-3">
-                        Paid
-                      </span>
-                    </div>
-                  ) : (
-                    ""
-                  )}
-                </div>
-              </td>
+      {assignments.length === 0 ? (
+        <section className="flex items-center h-full p-16">
+          <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
+            <div className="max-w-md text-center">
+              <span className="mb-8 font-extrabold text-3xl dark:text-gray-600">
+                Nothing to see here
+              </span>
+              <p className="text-sm font-semibold m-5">
+                <Link href={"/assignments/available"}>
+                  <a>Request new articles</a>
+                </Link>
+              </p>
+              <Link href={"/"}>
+                <a
+                  rel="noopener noreferrer"
+                  className="px-8 py-3 font-semibold rounded dark:bg-primary text-white"
+                >
+                  Back to homepage
+                </a>
+              </Link>
+            </div>
+          </div>
+        </section>
+      ) : (
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Title</th>
+              <th>Status</th>
+              <th>Due Date</th>
+              <th>Payment</th>
             </tr>
-          ))}
-        </tbody>
-        <tfoot>
-          <tr>
-            <th></th>
-            <th>Title</th>
-            <th>Status</th>
-            <th>Due Date</th>
-            <th>Payment</th>
-          </tr>
-        </tfoot>
-      </table>
+          </thead>
+          <tbody>
+            {assignments.map((assignment) => (
+              <tr key={assignment.id}>
+                <th className="text-xs">{serial++}</th>
+                <td>
+                  <div className="flex flex-col flex-wrap break-words">
+                    <div className="font-bold text-sm">
+                      <a href={"/assignments/" + assignment.id}>
+                        {assignment.title}
+                      </a>
+                    </div>
+                    <div className="text-xs opacity-50">
+                      For {assignment.client_name}
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  {assignment.published_url ? (
+                    <a
+                      className="link badge badge-success badge-lg text-sm text-white"
+                      href={assignment.published_url}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Published
+                    </a>
+                  ) : (
+                    <span
+                      className={`badge ${
+                        assignment.status === assignmentStatuses.assigning
+                          ? "badge-info"
+                          : "badge-primary"
+                      } badge-lg text-sm`}
+                    >
+                      {assignment.status}
+                    </span>
+                  )}
+                </td>
+                <td>
+                  <span className="text-sm">
+                    {dayjs(assignment.writer_due_date).format("LL")}
+                  </span>
+                </td>
+                <td>
+                  <div className="text-sm">
+                    ${assignment.writer_payout}{" "}
+                    {assignment.writer_paid_date ? (
+                      <div
+                        className="tooltip tooltip-left"
+                        data-tip={
+                          "Payment initiated on " +
+                          dayjs(assignment.writer_paid_date).format("LL")
+                        }
+                      >
+                        <span className="badge badge-ghost badge-lg text-sm ml-3">
+                          Paid
+                        </span>
+                      </div>
+                    ) : (
+                      ""
+                    )}
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+          <tfoot>
+            <tr>
+              <th></th>
+              <th>Title</th>
+              <th>Status</th>
+              <th>Due Date</th>
+              <th>Payment</th>
+            </tr>
+          </tfoot>
+        </table>
+      )}
     </div>
   );
 }
--- components/requests/requests.js
+++ components/requests/requests.js
@@ -1,13 +1,11 @@
-import { assignmentStatuses } from "../../constants/assignment-statuses";
-import { useState, useEffect } from "react";
 import * as dayjs from "dayjs";
 import * as localizedFormat from "dayjs/plugin/localizedFormat";
 import * as utc from "dayjs/plugin/utc";
-import { useAssignments } from "../../data/use-assignments";
 import { Error } from "../error";
 import { useRequests } from "../../data/use-requests";
 import Link from "next/link";
 import RequestStatus from "../badges/requests_status";
+
 dayjs.extend(localizedFormat);
 dayjs.extend(utc);
 
@@ -48,48 +46,73 @@ export default function Requests({ type }) {
   let serial = 1;
   return (
     <div className="overflow-x-auto">
-      <table className="table w-full">
-        <thead>
-          <tr>
-            <th></th>
-            <th>Assignment</th>
-            <th>Status</th>
-            <th>Due Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          {requests.map((request) => (
-            <tr key={request.id + request.assignment_id}>
-              <th className="text-xs">{serial++}</th>
-              <td>
-                <div className="flex flex-col flex-wrap break-words">
-                  <div className="font-bold text-sm">
-                    <Link href={"/assignments/" + request.assignment_id}>
-                      <a>{request.title}</a>
-                    </Link>
-                  </div>
-                </div>
-              </td>
-              <td>
-                <RequestStatus status={request.request_status} />
-              </td>
-              <td>
-                <span className="text-sm">
-                  {dayjs(request.writer_due_date).format("LL")}
-                </span>
-              </td>
+      {requests.length === 0 ? (
+        <section className="flex items-center h-full p-16">
+          <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
+            <div className="max-w-md text-center">
+              <span className="mb-8 font-extrabold text-3xl dark:text-gray-600">
+                Nothing to see here
+              </span>
+              <p className="text-sm font-semibold m-5">
+                <Link href={"/assignments/available"}>
+                  <a>Request new articles</a>
+                </Link>
+              </p>
+              <Link href={"/"}>
+                <a
+                  rel="noopener noreferrer"
+                  className="px-8 py-3 font-semibold rounded dark:bg-primary text-white"
+                >
+                  Back to homepage
+                </a>
+              </Link>
+            </div>
+          </div>
+        </section>
+      ) : (
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Assignment</th>
+              <th>Status</th>
+              <th>Due Date</th>
             </tr>
-          ))}
-        </tbody>
-        <tfoot>
-          <tr>
-            <th></th>
-            <th>Assignment</th>
-            <th>Status</th>
-            <th>Due Date</th>
-          </tr>
-        </tfoot>
-      </table>
+          </thead>
+          <tbody>
+            {requests.map((request) => (
+              <tr key={request.id + request.assignment_id}>
+                <th className="text-xs">{serial++}</th>
+                <td>
+                  <div className="flex flex-col flex-wrap break-words">
+                    <div className="font-bold text-sm">
+                      <Link href={"/assignments/" + request.assignment_id}>
+                        <a>{request.title}</a>
+                      </Link>
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  <RequestStatus status={request.request_status} />
+                </td>
+                <td>
+                  <span className="text-sm">
+                    {dayjs(request.writer_due_date).format("LL")}
+                  </span>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+          <tfoot>
+            <tr>
+              <th></th>
+              <th>Assignment</th>
+              <th>Status</th>
+              <th>Due Date</th>
+            </tr>
+          </tfoot>
+        </table>
+      )}
     </div>
   );
 }
